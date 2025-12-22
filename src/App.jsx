b5777d0e5597138f64af477f6344713717668f8b
Kirillv8 <urmancheev.it@gmail.com>
2025-12-22 23:51:00 +0300

    feat(widgets): implement movie filters sidebar with sorting and genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import AuthModal from "@widgets/autth/AuthModal";
 import Header from "@widgets/header/ui/Header";
+import Filters from "@widgets/filters/ui/Filters";
 function App() {
   return (
     <>
       {/* <AuthModal /> */}
       <Header />
+      <Filters />
     </>
   );
 }
