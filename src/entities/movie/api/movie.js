bd54f577f4a0c20a444514304c3f927e417ddfcd
kirillv8 <urmancheev.it@gmail.com>
2026-03-04 00:15:48 +0300

    fix: switch to discover endpoint for genre filtering

--- src/entities/movie/api/movie.js
+++ src/entities/movie/api/movie.js
@@ -3,16 +3,21 @@ const ENDPOINT_POPULARITY = import.meta.env.VITE_TMDB_ENDPOINT_POPULARITY;
 const ENDPOINT_TOP_RATING = import.meta.env.VITE_TMDB_ENDPOINT_TOP_RATED;
 
 export const movie = (sortBy, page, genres) => {
-  const endpoint =
+  const genreIds = genres?.map((g) => g.id).join(",");
+
+  let endpoint =
     sortBy === "popularity.desc" ? ENDPOINT_POPULARITY : ENDPOINT_TOP_RATING;
 
-  const genreId = genres
-    ?.map((genre) => {
-      return genre.id;
-    })
-    .join(",");
+  if (genreIds) {
+    endpoint = "/discover/movie";
+  }
 
   return apiInstance(endpoint, {
-    params: { page, language: "ru-RU", with_genres: genreId },
+    params: {
+      page,
+      language: "ru-RU",
+      sort_by: sortBy,
+      with_genres: genreIds,
+    },
   });
 };
